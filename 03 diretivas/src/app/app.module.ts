acd7a69a1bc02a23849d031959a5e445b94eb089
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-12-23 10:58:34 -0300

    Fim da Aula 33

--- 03 diretivas/src/app/app.module.ts
+++ 03 diretivas/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.co
 import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
 import { OperadorElvisComponent } from './operador-elvis/operador-elvis.component';
 import { ExemploNgContentComponent } from './exemplo-ng-content/exemplo-ng-content.component';
+import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
+import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { ExemploNgContentComponent } from './exemplo-ng-content/exemplo-ng-conte
     DiretivaNgclassComponent,
     DiretivaNgstyleComponent,
     OperadorElvisComponent,
-    ExemploNgContentComponent
+    ExemploNgContentComponent,
+    FundoAmareloDirective,
+    DiretivasCustomizadasComponent
   ],
   imports: [
     BrowserModule,
---
+++ 03 diretivas/src/app/shared/fundo-amarelo.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: 'p[fundoAmarelo]' //  o que vir entre '[ é o elemento afetado pela diretiva
+})
+export class FundoAmareloDirective {
+
+  constructor(
+    private _elementRef: ElementRef,
+    private _renderer: Renderer2,
+  ) {
+    // this._elementRef.nativeElement.style.backgroundColor = 'yellow'; // codigo não é recomendado por falha de segurança.. na versão 2
+    this._renderer.setStyle(this._elementRef.nativeElement, 'background-color', 'yellow');
+  }
+
+}
