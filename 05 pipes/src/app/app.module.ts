a1d693f2f35b9b7d791238825b444fca20a55f18
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-12-28 11:38:08 -0300

    Aula 45

--- 05 pipes/src/app/app.module.ts
+++ 05 pipes/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
+
+import ptBr from '@angular/common/locales/pt'; // necessário a partir do Angular v5
+import { registerLocaleData } from '@angular/common'; // necessário a partir do Angular v5
+registerLocaleData(ptBr); // necessário a partir do Angular v5
 
 @NgModule({
   declarations: [
@@ -14,7 +19,18 @@ import { CamelCasePipe } from './camel-case.pipe';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService: SettingsService) => settingsService.getLocale()
+    },
+    /*   {
+        provide: LOCALE_ID,
+        useValue: 'pt-BR',
+      }, */
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
