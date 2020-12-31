c3c7661afb600093b7e81fd8aa2cc562ae236ac1
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-12-31 11:32:17 -0300

    Fim da aula 69

--- 06 rotas/src/app/app.routing.module.ts
+++ 06 rotas/src/app/app.routing.module.ts
@@ -12,12 +12,12 @@ const appRoutes: Routes = [
   {
     path: 'cursos',
     loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule),
-    canActivate: [AuthGuard], canActivateChild: [CursosGuard],
+    canActivate: [AuthGuard], canActivateChild: [CursosGuard], canLoad: [AuthGuard],
   },
   {
     path: 'alunos',
     loadChildren: () => import('./alunos/alunos.module').then(m => m.AlunosModule),
-    canActivate: [AuthGuard], /*canActivateChild: [AlunosGuard],*/
+    canActivate: [AuthGuard], canLoad: [AuthGuard], /*canActivateChild: [AlunosGuard],*/
   },
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
--- 06 rotas/src/app/guards/auth.guard.ts
+++ 06 rotas/src/app/guards/auth.guard.ts
@@ -1,20 +1,39 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../login/auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(
     private authService: AuthService,
     private router: Router,
 
   ) { }
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+
+  private verificarAcesso(): boolean {
     if (this.authService.usuarioEstaAuntenticado()) return true;
 
     this.router.navigate(['/login'])
     return false;
+  }
+
+
+  canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.verificarAcesso();
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+    return this.verificarAcesso();
   }
 }
