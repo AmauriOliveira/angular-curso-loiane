23c7f31a22f7cef407dd27a9851fa37e61285eb5
Amauri Oliveira <amauriibate32@hotmail.com>
2021-01-04 13:49:25 -0300

    Fim da aula 79, 80, 81

--- 07 forms/src/app/app.module.ts
+++ 07 forms/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { TemplateFormModule } from './template-form/template-form.module';
 @NgModule({
   declarations: [
     AppComponent,
-    DataFormComponent
+    DataFormComponent,
   ],
   imports: [
     BrowserModule,
--- 07 forms/src/app/template-form/template-form.module.ts
+++ 07 forms/src/app/template-form/template-form.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { TemplateFormComponent } from './template-form.component';
-
+import { FormDebugComponent } from '../form-debug/form-debug.component';
 
 
 @NgModule({
   declarations: [
     TemplateFormComponent,
+    FormDebugComponent,
+
   ],
   imports: [
     CommonModule,
