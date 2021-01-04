678d6457ef88049b94081caaaa6800752d446269
Amauri Oliveira <amauriibate32@hotmail.com>
2021-01-04 10:54:02 -0300

    Fim da aula 74

--- 07 forms/src/app/template-form/template-form.component.ts
+++ 07 forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
@@ -12,4 +13,9 @@ export class TemplateFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSubmit(form: NgForm): void {
+    console.log('====================================');
+    console.log(form.value);
+    console.log('====================================');
+  }
 }
