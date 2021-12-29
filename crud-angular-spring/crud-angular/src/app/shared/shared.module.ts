ad221a7fb3bcc8169e42555712168c1d1a69243e
Amauri Oliveira <amauriibate32@hotmail.com>
2021-12-29 16:22:57 -0300

    Criado Pipe para  icons personalizado na categoria

--- crud-angular-spring/crud-angular/src/app/shared/shared.module.ts
+++ crud-angular-spring/crud-angular/src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
-import { AppMaterialModule } from './app-material/app-material.module';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AppMaterialModule } from './app-material/app-material.module';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
+import { CategoryPipe } from './pipes/category.pipe';
 
 @NgModule({
-  declarations: [ErrorDialogComponent],
+  declarations: [ErrorDialogComponent, CategoryPipe],
   imports: [CommonModule, AppMaterialModule],
-  exports: [ErrorDialogComponent],
+  exports: [CategoryPipe, ErrorDialogComponent],
 })
 export class SharedModule {}
