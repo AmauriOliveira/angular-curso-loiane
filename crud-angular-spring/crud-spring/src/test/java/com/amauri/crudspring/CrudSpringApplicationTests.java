51726c8ae050a12676927abd570067338f212747
Amauri Oliveira <amauriibate32@hotmail.com>
2022-12-01 09:23:27 -0300

    Add - Spring

---
+++ crud-angular-spring/crud-spring/src/test/java/com/amauri/crudspring/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amauri.crudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
