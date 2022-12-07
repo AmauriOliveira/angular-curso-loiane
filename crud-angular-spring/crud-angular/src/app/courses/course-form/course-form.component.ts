fa69a9ff39fa860e77c1ddf612fd6e4a468a1768
Amauri Oliveira <amauriibate32@hotmail.com>
2022-12-07 15:06:15 -0300

    Construindo o form de novo curso

--- crud-angular-spring/crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular-spring/crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null],
+    });
   }
 
+  ngOnInit(): void {
+    console.log('load');
+  }
+
+  onSubmit(): void {
+    console.log('submit');
+  }
+
+  onCancel(): void {
+    console.log('cancel');
+  }
 }
--- crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -2,20 +2,24 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
-    MatTableModule,
-    MatCardModule,
-    MatToolbarModule,
-    MatProgressSpinnerModule,
-    MatDialogModule,
     MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatToolbarModule,
   ],
 })
 export class AppMaterialModule {}
